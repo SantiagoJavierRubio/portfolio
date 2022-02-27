00f733d7298b2ce8e8b87b494e3b5ffea8b8612e
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-02-27 12:48:19 -0300

    section structure

---
+++ client/src/components/Home/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import './contact.css'
+
+const Contact = () => {
+    return(
+        <section id="contact">
+            <div id="questions">
+                <h6>Don't  have a website yet?</h6>
+                <h6>Looking for an upgrade on your existing web?</h6>
+                <h6>Want to improve your business?</h6>
+            </div>
+        </section>
+    )
+}
+
+export default Contact
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,37 @@
 import Featured from './Featured/Featured'
 import Intro from './Intro/Intro'
+import Contact from './Contact/Contact'
+
+import TopGreenCurve from '../SVGS/green_curve_top_3.svg'
+import BottomGreenCurve from '../SVGS/green_curve_bottom_1.svg'
+import TopPurpleCurve from '../SVGS/green_curve_top_3.svg'
+import BottomPurpleCurve from '../SVGS/green_curve_bottom_1.svg'
+import './home.css'
 
 const Home = () => {
     return(
-        <>
+        <div id="home">
             <Intro />
+            <div className="separatorCurve" id="featured-top" style={{
+                backgroundImage: `url(${TopGreenCurve})`
+            }}>         
+            </div>
             <Featured />
-        </>
-    )
-}
-
-export default Home
+            <div className="separatorCurve" id="featured-bottom" style={{
+                backgroundImage: `url(${BottomGreenCurve})`
+            }}>            
+            </div>
+            <div className="separatorCurve" id="featured-top" style={{
+                backgroundImage: `url(${TopPurpleCurve})`
+            }}>         
+            </div>
+            <Contact />
+            <div className="separatorCurve" id="featured-bottom" style={{
+                backgroundImage: `url(${BottomPurpleCurve})`
+            }}>            
+            </div>
+        </div>
+        )
+    }
+    
+    export default Home
