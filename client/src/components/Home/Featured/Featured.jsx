9df2aa8637bb4b4f5758fd39302dd78e522eee61
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-03-06 20:47:41 -0300

    trying api on frontend

--- client/src/components/Home/Featured/Featured.jsx
+++ client/src/components/Home/Featured/Featured.jsx
@@ -1,34 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PortfolioCard from '../../PortfolioCard/PortfolioCard'
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import './featured.css'
 
 
 const Featured = () => {
-    const testItems = [
-        {
-            name: 'Prueba 1',
-            thumbnail: 'https://i2.wp.com/dialoguemos.ec/wp-content/uploads/2016/04/thumbnail-default.jpg?ssl=1',
-            description: 'Esta es la prueba número uno, que como todos ya sabrán es la que siempre sale mal',
-            id: 1
-        },
-        {
-            name: 'Prueba2',
-            thumbnail: 'https://i2.wp.com/dialoguemos.ec/wp-content/uploads/2016/04/thumbnail-default.jpg?ssl=1',
-            description: 'Otra prueba, de otro tipo',
-            id: 2
-        },
-        {
-            name: 'Prueba 3',
-            thumbnail: 'https://i2.wp.com/dialoguemos.ec/wp-content/uploads/2016/04/thumbnail-default.jpg?ssl=1',
-            description: 'Prueba numero 3, esta si.',
-            id: 3
-        }
-    ]
+    const [entries, setEntries] = useState([])
+    const getFeaturedEntries = async() => {
+        const res = await axios.get('/api/featured')
+        if(res.data) setEntries(res.data)
+    }
+
+    useEffect(() => {
+       getFeaturedEntries()
+    }, [])
+
     return(
         <>
         <section id="featured">
-            {testItems.map(entry => {
-                return <PortfolioCard entry={entry} key={entry.id}/>
+            {entries && entries.map(entry => {
+                return <PortfolioCard entry={entry} key={entry._id}/>
             })}
         </section>
         <div id="goToPortfolio">
